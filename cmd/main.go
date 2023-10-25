8d57182075286c1e8901e0fb8cb796ec4cbe1220
aspandyar <aspandyart@gmail.com>
2023-10-25 21:54:45 +0600

    cute interface

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,17 @@ package main
 import (
 	"github.com/aspandyar/todo-list"
 	"github.com/aspandyar/todo-list/pkg/handler"
+	"github.com/aspandyar/todo-list/pkg/repository"
+	"github.com/aspandyar/todo-list/pkg/service"
 	"log"
 )
 
 func main() {
 	srv := new(todo.Server)
 
-	handlers := new(handler.Handler)
+	repo := repository.NewRepository()
+	services := service.NewService(repo)
+	handlers := handler.NewHandler(services)
 
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
