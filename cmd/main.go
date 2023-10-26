cedb09852853daa0769256ea391b95ee7f34e344
aspandyar <aspandyart@gmail.com>
2023-10-26 23:28:25 +0600

    +postgreSQL

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,51 @@
 package main
 
 import (
-	"github.com/aspandyar/todo-list"
+	"github.com/aspandyar/todo-list/internal"
 	"github.com/aspandyar/todo-list/pkg/handler"
 	"github.com/aspandyar/todo-list/pkg/repository"
 	"github.com/aspandyar/todo-list/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
 	"log"
+	"os"
 )
 
 func main() {
-	srv := new(todo.Server)
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: #{err.Error()}")
+	}
 
-	repo := repository.NewRepository()
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	srv := new(internal.Server)
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		log.Fatalf("failed to initalize bd: %s", err.Error())
+	}
+
+	repo := repository.NewRepository(db)
 	services := service.NewService(repo)
 	handlers := handler.NewHandler(services)
 
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
