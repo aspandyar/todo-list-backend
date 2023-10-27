6e69c6f9b3c0dd468034e672e011e025b237862c
aspandyar <aspandyart@gmail.com>
2023-10-27 17:12:41 +0600

    JWT tokens and sign in method

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -5,9 +5,20 @@ import (
 	"fmt"
 	"github.com/aspandyar/todo-list/internal/models"
 	"github.com/aspandyar/todo-list/pkg/repository"
+	"github.com/dgrijalva/jwt-go"
+	"time"
 )
 
-const salt = "ajhdajsdh1lk23m1j3n1"
+const (
+	salt       = "ajhdajsdh1lk23m1j3n1"
+	signingKey = "akjdnqkenj3hqruiwyu@q8eh@#du"
+	tokenTTL   = 12 * time.Hour
+)
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
+}
 
 type AuthService struct {
 	repo repository.Authorization
@@ -21,6 +32,23 @@ func (s *AuthService) CreateUser(user models.User) (int, error) {
 	user.Password = generatePasswordHash(user.Password)
 
 	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, generatePasswordHash(password))
+	if err != nil {
+		return "", nil
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		UserId: user.Id,
+	})
+
+	return token.SignedString([]byte(signingKey))
 }
 
 func generatePasswordHash(password string) string {
